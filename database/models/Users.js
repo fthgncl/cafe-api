c24abd74af5b8bc87b4aac5d0a92f9724315cdfa
fthgncl <gencal.fatih61@gmail.com>
2024-08-26 02:34:36 +0300

    kullanıcı adı boşluk içermeyecek şekilde güncellendi

--- database/models/Users.js
+++ database/models/Users.js
@@ -4,18 +4,19 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
     firstname: {
         type: String, required: true,
-        maxlength: [20, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
-        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
+        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır']
     },
     lastname: {
         type: String, required: true,
-        maxlength: [20, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
-        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
+        minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır']
     },
     username: {
         type: String, required: true, unique: true,
-        maxlength: [20, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
+        maxlength: [15, '`{PATH}` alanı (`{VALUE}`), ({MAXLENGTH}) karakterden küçük olmalıdır'],
         minlength: [3, '`{PATH}` alanı (`{VALUE}`), ({MINLENGTH}) karakterden büyük olmalıdır'],
+        match: [/^\S*$/, '`{PATH}` alanı boşluk içermemelidir']
     },
     phone: {type: String, required: true, unique: true},
     password: {type: String, minlength: 6, required: true},
